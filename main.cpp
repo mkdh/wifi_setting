b5faff3f89c2db37a539ffad14ba882a16d494db
mkdh <mkdh321@yahoo.com.tw>
2017-03-24 17:52:45 +0800

    [error] open wifi page on main()
    [error] open wifi page on main()

--- main.cpp
+++ main.cpp
@@ -13,13 +13,37 @@ int main(int argc, char *argv[])
 
 
     qDebug() << __func__;
+    /*
+
     QAndroidJniObject javaNotification = QAndroidJniObject::fromString("QQQ");
     QAndroidJniObject::callStaticMethod<void>("PopUpWiFiSettingPage",
                                        "notify",
                                        "(Ljava/lang/String;)V",
                                        javaNotification.object<jstring>());
-    qDebug() << "=======================";
-    /*
+    */
+
+    //startActivity(new Intent(Settings.ACTION_WIFI_SETTINGS));
+    //http://stackoverflow.com/questions/25024615/open-android-settings-from-qt-appcom-android-settings
+    QAndroidJniObject activity = QAndroidJniObject::callStaticObjectMethod("org/qtproject/qt5/android/QtNative", "activity", "()Landroid/app/Activity;");   //activity is valid
+    if ( activity.isValid() )
+    {
+        // Equivalent to Jave code: 'Intent intent = new Intent();'
+        //http://doc.qt.io/qt-5/qandroidjniobject.html#QAndroidJniObject-4
+        QAndroidJniObject intent("android/content/Intent","(I)V", "Settings.ACTION_WIFI_SETTINGS");
+        if ( intent.isValid() )
+        {
+            activity.callObjectMethod("startActivity","(Landroid/content/Intent;)V",intent.object<jobject>());
+        }
+    }
+
+
+    /*method 1
+
+    //startActivity(new Intent(Settings.ACTION_WIFI_SETTINGS));
+    //http://stackoverflow.com/questions/25024615/open-android-settings-from-qt-appcom-android-settings
+
+    //crash code
+    // It will make my app crash at "activity.callObjectMethod....." line
     QAndroidJniObject activity = QAndroidJniObject::callStaticObjectMethod("org/qtproject/qt5/android/QtNative", "activity", "()Landroid/app/Activity;");   //activity is valid
     if ( activity.isValid() )
     {
@@ -27,12 +51,13 @@ int main(int argc, char *argv[])
         QAndroidJniObject intent("android/content/Intent","()V");
         if ( intent.isValid() )
         {
+            //http://stackoverflow.com/questions/11429090/how-to-open-wifi-settings-on-nook-nookcolor
             QAndroidJniObject param1 = QAndroidJniObject::fromString("com.android.settings");
-            QAndroidJniObject param2 = QAndroidJniObject::fromString("com.android.settings.DevelopmentSettings");
+            QAndroidJniObject param2 = QAndroidJniObject::fromString("com.android.se‌​ttings.wifi.Settings‌​_Wifi_Settings");
 
             if ( param1.isValid() && param2.isValid() )
             {
-                // Equivalent to Jave code: 'intent.setClassName("com.android.settings", "com.android.settings.DevelopmentSettings");'
+                // Equivalent to Jave code: 'intent.setClassName("com.android.phone", "com.android.phone.NetworkSetting");'
                 intent.callObjectMethod("setClassName","(Ljava/lang/String;Ljava/lang/String;)Landroid/content/Intent;",param1.object<jobject>(),param2.object<jobject>());
 
                 // Equivalent to Jave code: 'startActivity(intent);'
@@ -41,5 +66,8 @@ int main(int argc, char *argv[])
         }
     }
     */
+
+    qDebug() << "=======================";
+
     return app.exec();
 }
